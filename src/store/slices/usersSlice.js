4d5ada220f20bfbbdf67a99fd138d4e7c55616fe
shreesh30 <shreesharya20@gmail.com>
2023-07-23 03:41:38 +0530

    added thunks

--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -1,11 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchUsers } from "../thunks/fetchUsers";
 
 const usersSlice = createSlice({
   name: "users",
   initialState: {
     data: [],
+    isLoadng: false,
+    error: null,
   },
-  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchUsers.pending, (state, action) => {
+      state.isLoadng = true;
+    });
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      state.isLoadng = false;
+      state.data = action.payload;
+    });
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      state.isLoadng = false;
+      state.error = action.error;
+    });
+  },
 });
 
 export const usersReducer = usersSlice.reducer;
